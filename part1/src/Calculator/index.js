ea3f07d63704ef833be20c7f98abd0847c129959
John  Tucker <john@larkintuckerllc.com>
2018-01-28 17:33:30 -0500

    First commit

---
+++ part1/src/Calculator/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './Button';
+
+function Calculator() {
+  return (
+    <div>
+      <Button number={0} />
+    </div>
+  );
+}
+export default Calculator;
