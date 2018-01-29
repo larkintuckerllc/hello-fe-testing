b22a71031acd8479d70167237df027277931c40b
John  Tucker <john@larkintuckerllc.com>
2018-01-29 10:21:11 -0500

    Enzyme

--- part2/src/Calculator/Button/Button.test.jsx
+++ part2/src/Calculator/Button/Button.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Button from '../Button';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button number={0} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+it('shallow renders without crashing', () => {
+  shallow(<Button number={0} />);
 });
--- part2/src/Calculator/Calculator.test.jsx
+++ part2/src/Calculator/Calculator.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Calculator from '../Calculator';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Calculator />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+it('shallow renders without crashing', () => {
+  shallow(<Calculator />);
 });
