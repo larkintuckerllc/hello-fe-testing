a3bfb91b270dae060ce3e7f7c1c21fa74b606f0f
John  Tucker <john@larkintuckerllc.com>
2018-01-30 20:19:08 -0500

    Fixed

--- part3/src/MyApp/components/Calculator/Display/Display.test.jsx
+++ part3/src/MyApp/components/Calculator/Display/Display.test.jsx
@@ -4,9 +4,11 @@ import Adapter from 'enzyme-adapter-react-16';
 import { Display } from '../Display';
 
 Enzyme.configure({ adapter: new Adapter() });
-it('shallow renders with number without crashing', () => {
-  shallow(<Display first={1} />);
-});
-it('shallow renders with null without crashing', () => {
-  shallow(<Display first={null} />);
+describe('shallow renders without crashing', () => {
+  it('shallow renders without crashing with number', () => {
+    shallow(<Display first={1} />);
+  });
+  it('shallow renders without crashing with null', () => {
+    shallow(<Display first={null} />);
+  });
 });
